55b3bf9b8d0384d4347b264314e971ed6f5d1a80
ThePotatoMan <potato.man@thepotatochurch.com>
2022-04-16 17:09:26 +0100

    Added support for 'YYYY/MM/DD' version format. Changed to logging instead of printing.
    Signed-off-by: ThePotatoMan <potato.man@thepotatochurch.com>

--- code/version.py
+++ code/version.py
@@ -9,12 +9,20 @@ def get_version(logger: logging.Logger = null_logger) -> str:
 	version_file = open(version_file_name, "r")
 
 	loaded_version_string = version_file.readline()
-	version_string_pattern = r"[0-9]+\.[0-9]+\.[0-9]+"
+	version_string_pattern1 = r"[0-9]+\.[0-9]+\.[0-9]+"  # eg. 1.10.0
+	version_string_pattern2 = r"^\d{4}/(0[1-9]|1[012])/(0[1-9]|[12][0-9]|3[01])$"  # eg. 2022/04/16
 
-	does_version_string_match_pattern = re.match(version_string_pattern, loaded_version_string)
+	does_version_string_match_pattern1 = re.match(version_string_pattern1, loaded_version_string)
+	does_version_string_match_pattern2 = re.match(version_string_pattern2, loaded_version_string)
+	does_version_string_match_pattern = does_version_string_match_pattern1 or does_version_string_match_pattern2
+
 	if does_version_string_match_pattern:
-		print("Obtained version string.")
+		if does_version_string_match_pattern1:
+			logger.debug(f"Obtained version string matching the classic pattern. Version: `{loaded_version_string}`")
+		if does_version_string_match_pattern2:
+			logger.debug(f"Obtained version string matching the YYYY/MM/DD pattern. Version: `{loaded_version_string}`")
 	else:
-		print("Obtained version string, but it does not match the expected pattern.")
+		logger.warning(f"Obtained version string, but it does not match any expected pattern. "
+		               f"Version: `{loaded_version_string}`")
 
 	return loaded_version_string
